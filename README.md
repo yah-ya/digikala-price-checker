9c2c1a601fb56bde136562fd54b5fcf9ae19ddf4
Yahyya <y.t.15132@gmail.com>
2022-02-27 16:15:16 +0330

    Fixed issues

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Change the product lists in index.php at line 4 ($productCodes). Only include the number of the product , NOT THE "DKP"
 run index.php
+
+Dont Forget to change $thisSellerId to your Seller ID .
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 
 include 'curl.php';
+$thisSellerId = '';
 $productCodes = [
-    "7310155","5755172","5472330"
+    "7310155"
 ];
 
 foreach($productCodes as $code){
-    doCurl(url:"https://api.digikala.com/v1/product/".$code.'/',callback: function($res){
+    doCurl(url:"https://api.digikala.com/v1/product/".$code.'/',callback: function($res) use ($thisSellerId) {
         $res = json_decode($res);
         print $res->data->product->title_fa;
-        print  " -> Seller : ".$res->data->product->default_variant->seller->title;
+        print  " -> Seller : <b><span style='".($res->data->product->default_variant->seller->id==$thisSellerId ? 'color:green' : 'color:red' )."'>" .$res->data->product->default_variant->seller->title.'</span></b>';
         print " -> Price : " . $res->data->product->default_variant->price->selling_price;
         print "<br />".PHP_EOL;
     });
